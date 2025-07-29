7ae2aacdb411fb6edadddb8181a75d466c1a1559
kennykee <kennykee@kennykee.com>
2025-07-29 19:25:14 +0800

    First commit

---
+++ models/item.js
@@ -0,0 +1,156 @@
+var items = {
+    "Glass":[
+        {"image":"Beer-Bottle.png",     "fact":"A glass bottle could take over 4000 years to decompose."},
+        {"image":"Bowl.png",            "fact":""},
+        {"image":"Fish-Tank.png",       "fact":""},
+        {"image":"Glass-Jar.png",       "fact":""},
+        {"image":"Glass-Mug.png",       "fact":""},
+        {"image":"Glass-Plate.png",     "fact":""},
+        {"image":"Icecream-Glass.png",  "fact":""},
+        {"image":"Jam-Jar.png",         "fact":""},
+        {"image":"Jar.png",             "fact":""},
+        {"image":"Perfume-Bottle.png",  "fact":""},
+        {"image":"Serving-Bowl.png",    "fact":""},
+        {"image":"Short-Glass.png",     "fact":""},
+        {"image":"White-Bottle.png",    "fact":"Recycling one glass bottle saves enough energy to power a 100-watt bulb for four hours."},
+        {"image":"Wine-Glass.png",      "fact":""}
+    ], 
+    "Metal":[
+        {"image":"Aluminum-Can.png",    "fact":"Aluminum cans can be recycled and put back onto the shelf at your local grocery store in just about 2 months."},
+        {"image":"Biscuit-Tin.png",     "fact":""},
+        {"image":"Bottle-Cap.png",      "fact":""},
+        {"image":"Buckle.png",          "fact":""},
+        {"image":"Chain.png",           "fact":""},
+        {"image":"Chain-Hook.png",      "fact":""},
+        {"image":"Crushed-Can.png",     "fact":"Recycling one aluminum can saves enough energy to run a TV for 3 hours!"},
+        {"image":"Handle-Bar.png",      "fact":""},
+        {"image":"Hook.png",            "fact":""},
+        {"image":"Metal.png",           "fact":""},
+        {"image":"Metal-Tag.png",       "fact":""},
+        {"image":"Metal-Tin.png",       "fact":""},
+        {"image":"Open-Tin.png",        "fact":""},
+        {"image":"Pot.png",             "fact":""},
+        {"image":"Round-Tin.png",       "fact":""},
+        {"image":"Spring.png",          "fact":""},
+        {"image":"Valve.png",           "fact":""}
+    ],
+    "Non-Recyclable":[
+        {"image":"Aluminum-Tray.png",   "fact":""},
+        {"image":"Batteries.png",       "fact":""},
+        {"image":"Disposible-Cutlery.png",   "fact":""},
+        {"image":"Flower-Pot.png",      "fact":""},
+        {"image":"Milk-Carton.png",     "fact":""},
+        {"image":"Paper-Bento-Boxes.png",   "fact":""},
+        {"image":"Pizza-Box.png",       "fact":""},
+        {"image":"Plastic-Bag.png",     "fact":"In Singapore, we use about 3 billion plastic bags a year."},
+        {"image":"PVC-Packaging.png",   "fact":""},
+        {"image":"Styrofoam.png",       "fact":"Styrofoam takes 500 years to decompose!"}
+    ],
+    "Organic-Waste":[
+        {"image":"Apple-Core.png",      "fact":"Organic garbage could be composted and recycled to use for fertilizer."},
+        {"image":"Banana-Peel.png",     "fact":"Composting can clean contaminated soil."},
+        {"image":"Bay-Leaves.png",      "fact":""},
+        {"image":"Carrot-Peel.png",     "fact":""},
+        {"image":"Dried-Rose.png",      "fact":""},
+        {"image":"Egg-Shell.png",       "fact":""},
+        {"image":"Fish-Bone.png",       "fact":""},
+        {"image":"Lemon.png",           "fact":""},
+        {"image":"Orange-Peel.png",     "fact":""},
+        {"image":"Tea-Bag.png",         "fact":""}
+    ],
+    "Paper":[
+        {"image":"Books.png",           "fact":""},
+        {"image":"Box.png",             "fact":""},
+        {"image":"Cardboard.png",       "fact":""},
+        {"image":"Coloured-Paper.png",  "fact":"900kg of recycled paper can help to save 17 trees."},
+        {"image":"Corn-Flakes.png",     "fact":""},
+        {"image":"Crushed-Paper.png",   "fact":""},
+        {"image":"Egg-Carton.png",      "fact":""},
+        {"image":"Envelope.png",        "fact":""},
+        {"image":"Folded-Cardboard.png","fact":""},
+        {"image":"Magazine.png",        "fact":""},
+        {"image":"Newspaper.png",       "fact":"24 trees are cut down to make 900kg of newspaper."},
+        {"image":"Notebook.png",        "fact":""},
+        {"image":"Packaging-Tray.png",  "fact":""},
+        {"image":"Paper-Bag.png",       "fact":""},
+        {"image":"Spaghetti-Box.png",   "fact":""},
+        {"image":"Tissue-Box.png",      "fact":""},
+        {"image":"Toilet-Roll.png",     "fact":""}
+    ],
+    "Plastic":[
+        {"image":"Air-Tight-Container.png", "fact":""},
+        {"image":"Basket.png",              "fact":""},
+        {"image":"Blocks.png",              "fact":""},
+        {"image":"Bottle.png",              "fact":""},
+        {"image":"CD-Cover.png",            "fact":""},
+        {"image":"Detergent-Bottle.png",    "fact":""},
+        {"image":"File.png",                "fact":""},
+        {"image":"House.png",               "fact":""},
+        {"image":"Ink-Catridge.png",        "fact":""},
+        {"image":"Milk-Bottle.png",         "fact":"Recycling 900kg of plastic can save up to 3780 to 7570 litres of gasoline."},
+        {"image":"Pail.png",                "fact":""},
+        {"image":"Plastic-Packaging.png",   "fact":""},
+        {"image":"Plate.png",               "fact":""},
+        {"image":"Stool.png",               "fact":""},
+        {"image":"Toy-Plane.png",           "fact":""},
+        {"image":"Water-Bottle.png",        "fact":"900kg of recycled plastic saves 5,774 kWh of electricity."},
+        {"image":"Water-Dispenser-Bottle.png",  "fact":""}
+    ],
+    "Textile":[
+        {"image":"Bag.png",                 "fact":""},
+        {"image":"Bath-Towel.png",          "fact":""},
+        {"image":"Jean.png",                "fact":"Nearly 100 percent of textiles and clothing are recyclable."},
+        {"image":"Leather-Shoe.png",        "fact":""},
+        {"image":"Pillow.png",              "fact":""},
+        {"image":"Rope.png",                "fact":""},
+        {"image":"Sack-Bag.png",            "fact":""},
+        {"image":"Scarf.png",               "fact":""},
+        {"image":"Shoe.png",                "fact":""},
+        {"image":"Socks.png",               "fact":""},
+        {"image":"Tote-Bag.png",            "fact":""},
+        {"image":"Towel.png",               "fact":""},
+        {"image":"T-Shirt.png",             "fact":"Over 700 gallons of water (enough to fill 22 bathtubs) are used to manufacture a single cotton T-shirt."}
+    ],
+    "Toxic-Waste":[
+        {"image":"Battery.png",             "fact":""},
+        {"image":"Bulb.png",                "fact":""},
+        {"image":"Gas-Cylinder.png",        "fact":""},
+        {"image":"Insect-Killer.png",       "fact":"5 - 15% of what we throw away contains hazardous substances."},
+        {"image":"Jerry-Can.png",           "fact":""},
+        {"image":"Motor-Oil.png",           "fact":"A single quart of motor oil, if disposed of improperly, can contaminate up to 2,000,000 gallons of fresh water."},
+        {"image":"Mr-Muscle.png",           "fact":""},
+        {"image":"OPI-Nail.png",            "fact":""},
+        {"image":"Paint.png",               "fact":""},
+        {"image":"Photo-Chemical.png",      "fact":""},
+        {"image":"Spray-Can.png",           "fact":""}
+    ]
+};
+
+var types = ["Paper","Plastic","Metal","Textile","Glass","Organic-Waste","Toxic-Waste","Non-Recyclable"];
+
+var levels = {
+    0:{
+        type:["Paper","Plastic","Metal"],
+        time_limit:60,
+        item_count: 12,
+        item_count_multiplier: 30,
+        point_correct:5,
+        point_incorrect:1
+    },
+    1:{
+        type:["Paper","Plastic","Textile","Glass"],
+        time_limit:60,
+        item_count: 12,
+        item_count_multiplier: 30,
+        point_correct:5,
+        point_incorrect:1
+    },
+    2:{
+        type:["Paper","Plastic","Organic-Waste","Toxic-Waste","Non-Recyclable"],
+        time_limit:60,
+        item_count: 10,
+        item_count_multiplier: 30,
+        point_correct:5,
+        point_incorrect:1
+    }
+};
